8de41824f025f3ba28e76a75d301d139badb2b5b
U190709055 <mehmetcantekgul@posta.mu.edu.tr>
2022-04-07 15:09:50 +0300

    lab8

--- lab8/TestMyDateTime.java
+++ lab8/TestMyDateTime.java
@@ -3,16 +3,16 @@ public class TestMyDateTime {
 
 
 	public static void main(String[] args) {
-		
-		MyTime time = new MyTime(15,30);
-		
-		MyDate date = new MyDate(28,2,2017);
-		
+
+		MyTime time = new MyTime(15, 30);
+
+		MyDate date = new MyDate(28, 2, 2017);
+
 		MyDateTime dateTime = new MyDateTime(date, time);
 		String strRep = dateTime.toString();
 		System.out.println(strRep);
 		//Expected output: 2017-02-28 15:30
-				
+
 		dateTime.incrementDay();
 		strRep = dateTime.toString();
 		System.out.println(strRep);
@@ -22,12 +22,12 @@ public class TestMyDateTime {
 		strRep = dateTime.toString();
 		System.out.println(strRep);
 		//Expected output: 2017-03-01 16:30
-		
+
 		dateTime.incrementHour(32);
 		strRep = dateTime.toString();
 		System.out.println(strRep);
 		//Expected output: 2017-03-03 00:30
-		
+
 		dateTime.decrementHour(49);
 		strRep = dateTime.toString();
 		System.out.println(strRep);
@@ -102,25 +102,25 @@ public class TestMyDateTime {
 		
 		MyDateTime anotherDateTime = new MyDateTime(new MyDate(29,3,2017), new MyTime(15,30));
 		
-		boolean before = dateTime.isBefore(anotherDateTime);
-		System.out.println(dateTime.toString()+ " is before " + anotherDateTime.toString()+ " = " + before);
+		//boolean before = dateTime.isBefore(anotherDateTime);
+		//System.out.println(dateTime.toString()+ " is before " + anotherDateTime.toString()+ " = " + before);
 		//Expected output: 2017-03-29 00:30 is before 2017-03-29 15:30 = true		
 		
-		boolean after = dateTime.isAfter(anotherDateTime);
-		System.out.println(dateTime.toString()+ " is after " + anotherDateTime.toString()+ " = " + after);
+		//boolean after = dateTime.isAfter(anotherDateTime);
+		//System.out.println(dateTime.toString()+ " is after " + anotherDateTime.toString()+ " = " + after);
 		//Expected output: 2017-03-29 00:30 is after 2017-03-29 15:30 = false
 		
-		String dayTimeDiff = dateTime.dayTimeDifference(anotherDateTime);
-		System.out.print("Day time difference between " + dateTime.toString()+ " and " + anotherDateTime.toString()+ " is " + dayTimeDiff);
+	//	String dayTimeDiff = dateTime.dayTimeDifference(anotherDateTime);
+	//	System.out.print("Day time difference between " + dateTime.toString()+ " and " + anotherDateTime.toString()+ " is " + dayTimeDiff);
 		//Expected output: Day difference between 2017-03-29 00:30 and 2017-03-29 15:30 is 15 hour(s)
 
-		anotherDateTime.incrementMinute(4850);
-		strRep = anotherDateTime.toString();
-		System.out.println(strRep);
+	//	anotherDateTime.incrementMinute(4850);
+	//	strRep = anotherDateTime.toString();
+	//	System.out.println(strRep);
 		//Expected output: 2017-04-02 00:20
 		
-		dayTimeDiff = dateTime.dayTimeDifference(anotherDateTime);
-		System.out.print("Day time difference between " + dateTime.toString()+ " and " + anotherDateTime.toString()+ " is " + dayTimeDiff);
+	//	dayTimeDiff = dateTime.dayTimeDifference(anotherDateTime);
+	//	System.out.print("Day time difference between " + dateTime.toString()+ " and " + anotherDateTime.toString()+ " is " + dayTimeDiff);
 		//Expected output: Day difference between 2017-03-29 00:30 and 2017-04-02 00:20 is 3 day(s) 23 hour(s) 50 minute(s)
 		
 	}
