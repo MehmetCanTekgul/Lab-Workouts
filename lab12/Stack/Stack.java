cf46788f7240ccc156bdd9cc3f548fc155ee83cb
190709055 <mehmetcantekgul@posta.mu.edu.tr>
2022-04-28 14:57:12 +0300

    lab12

---
+++ lab12/Stack/Stack.java
@@ -0,0 +1,8 @@
+package Stack;
+
+public interface Stack{
+        void push(Object item);
+        Object pop();
+        boolean empty();
+    }
+
---
+++ lab12/Stack/TestStack.java
@@ -0,0 +1,21 @@
+package Stack;
+
+public class TestStack {
+    public static void main(String[] args){
+        testStack(new Stackimpt());
+
+        testStack(new StackArrayListImpt());
+
+    } public static void testStack(Stack stack){
+        stack.push(5);
+        stack.push(6);
+        stack.push(2);
+        stack.push(11);
+        stack.push(23);
+
+        while (!stack.empty()){
+            System.out.println(stack.pop());
+        }
+    }
+
+}
